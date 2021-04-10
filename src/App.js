98b1fd9609d281f48eb1cb221a1353c2df432084
owlfinger <51793573+owlfinger@users.noreply.github.com>
2021-04-10 16:19:30 -0500

    debug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -8,33 +6,29 @@ import {
   Link
 } from "react-router-dom";
 
-function App() {
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Bill was here.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-        <Router>
+    <Router>
       <div>
         <ul>
           <li>
-            <Link to="/tic">tic</Link>
+            <Link to="/">Home</Link>
           </li>
-          
+          <li>
+            <Link to="./Tic.js">tic</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
         </ul>
 
         <hr />
@@ -47,19 +41,29 @@ function App() {
           of them to render at a time
         */}
         <Switch>
-          
+          <Route exact path="/">
+            <Home />
+          </Route>
           <Route path="/tic">
             <tic />
           </Route>
-          
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
         </Switch>
       </div>
     </Router>
   );
-        
-      </header>
-    </div>
+}
 
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
   );
 }
 
@@ -71,5 +75,10 @@ function tic() {
   );
 }
 
-
-export default App;
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +17,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>, 
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
