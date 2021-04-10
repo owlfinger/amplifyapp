9a8e2d9721be69427a4e1a1e0a6336bf3474ff92
owlfinger <51793573+owlfinger@users.noreply.github.com>
2021-04-10 11:46:48 -0500

    trying routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
@@ -20,17 +27,47 @@ function App() {
         >
           Learn React
         </a>
-        <a
-          className="App-link"
-          href="https://main.dqiratjava8y5.amplifyapp.com/tic.js"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tic-Tak-Toe
-        </a>
+        
+        <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/tic">tic</Link>
+          </li>
+          
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          
+          <Route path="/tic">
+            <tic />
+          </Route>
+          
+        </Switch>
+      </div>
+    </Router>
+  );
+        
       </header>
     </div>
 
+  );
+}
+
+function tic() {
+  return (
+    <div>
+      <h2>tic</h2>
+    </div>
   );
 }
 
